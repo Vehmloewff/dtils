790ca5c5bf30b842d4cc8698ab2767869867fc78
Elijah Mooring <vehmloewff@gmail.com>
2023-05-23 21:12:11 -0700

    ref!: remove number.multipyString

--- number.ts
+++ number.ts
@@ -1,22 +1,10 @@
-import { repeat } from './loop.ts'
-
-/**
- * Sets the amount of zeros before a number.  If `digits` is `null`, there will be no extra digits in front of the number
- */
+/** Sets the amount of zeros before a number.  If `digits` is `null`, there will be no extra digits in front of the number */
 export function fixedStartDigits(number: number, digits: number | null): string {
 	if (!digits) return String(number)
 
 	const str = String(number)
-	if (str.length < digits) return `${multiplyString(`0`, digits - str.length)}${str}`
+	if (str.length < digits) return `${`0`.repeat(digits - str.length)}${str}`
 	else return str
-}
-
-export function multiplyString(string: string, number: number): string {
-	let str = ``
-
-	repeat(number, () => (str += string))
-
-	return str
 }
 
 export function wrap(num: number, min: number, max: number) {
