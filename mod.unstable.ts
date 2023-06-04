a946289d2e13f0eb587d7284702182f0cb75d7d1
Elijah Mooring <vehmloewff@gmail.com>
2023-06-03 19:49:40 -0700

    ref: remove that annoying unstable warning logged

--- mod.unstable.ts
+++ mod.unstable.ts
@@ -6,10 +6,6 @@
  * Entrypoint for all existing and unstable Dtils apis
  */
 
-import { colors } from './deps.ts'
-
-console.warn(colors.yellow(colors.bold('CAUTION')), 'deno module is unstable')
-
 export * from './mod.ts'
 
 export * from './deno.ts'
