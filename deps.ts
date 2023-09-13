dd257f5a363eb67d0b67b8e989bbd682273ef4c4
Elijah Mooring <vehmloewff@gmail.com>
2023-09-13 12:41:16 -0700

    chore(deps): add porter dev dep

--- deps.ts
+++ deps.ts
@@ -11,3 +11,4 @@ export * as cbor from 'https://deno.land/x/cbor@v1.5.4/index.js'
 
 // Development
 export * as asserts from 'https://deno.land/std@0.201.0/assert/mod.ts'
+export * as porter from 'https://deno.land/x/port@1.0.0/mod.ts'
