dabf1192441841b6eecc2123fa865ca0f0d3be94
Elijah Mooring <vehmloewff@gmail.com>
2023-09-13 09:38:34 -0700

    feat(safe_unknown): add several methods to SafeUnknownObject and SafeUnknownArray

--- safe_unknown.test.ts
+++ safe_unknown.test.ts
@@ -1,5 +1,6 @@
 import { asserts } from './deps.ts'
-import { SafeUnknown, SafeUnknownObject } from './safe_unknown.ts'
+import { SafeUnknown, SafeUnknownArray, SafeUnknownObject } from './safe_unknown.ts'
+import { concatenate } from './string.ts'
 
 Deno.test('SafeUnknown gets the correct type, and throws if it\'s the wrong type', () => {
 	interface TypeTest {
@@ -110,4 +111,57 @@ Deno.test('SafeUnknownObject.sureGet properly gets', () => {
 Deno.test('SafeUnknownObject.sureGet can get single values', () => {
 	asserts.assertEquals(new SafeUnknownObject({ foo: 12 }).sureGet('foo').asNumber(), 12)
 	asserts.assertEquals(new SafeUnknownObject({ foo: { bar: 12 } }).sureGet('foo').isNumber(), false)
+})
+
+const ARRAY_TEST = ['foo', 'bar', 'bin', 'baz']
+const OBJECT_TEST = { foo: 'whatever, man', bar: 'sleep tight', bin: 'never rest', baz: 'be happy' }
+
+Deno.test('SafeUnknownArray.map maps', () => {
+	asserts.assertEquals(new SafeUnknownArray(ARRAY_TEST).map((v) => v.asString()), ARRAY_TEST)
+})
+
+Deno.test('SafeUnknownArray.forEach loops over each in order', () => {
+	let trueIndex = 0
+	new SafeUnknownArray(ARRAY_TEST).forEach((item, index) => {
+		asserts.assertEquals(index, trueIndex)
+		asserts.assertEquals(item.asString(), ARRAY_TEST[trueIndex])
+
+		trueIndex++
+	})
+
+	asserts.assertEquals(trueIndex, ARRAY_TEST.length)
+})
+
+Deno.test('SafeUnknownArray.values just gets some values', () => {
+	asserts.assertEquals(new SafeUnknownArray(ARRAY_TEST).values().map((v) => v.asString()), ARRAY_TEST)
+})
+
+Deno.test('SafeUnknownObject.keys just gets the object keys', () => {
+	asserts.assertEquals(new SafeUnknownObject(OBJECT_TEST).keys(), Object.keys(OBJECT_TEST))
+})
+
+Deno.test('SafeUnknownObject.values just gets the object values', () => {
+	asserts.assertEquals(new SafeUnknownObject(OBJECT_TEST).values().map((v) => v.asString()), Object.values(OBJECT_TEST))
+})
+
+Deno.test('SafeUnknownObject.forEach loops through object', () => {
+	const trueKeys = Object.keys(OBJECT_TEST)
+	const loopedKeys: string[] = []
+
+	new SafeUnknownObject(OBJECT_TEST).forEach((value, key) => {
+		// @ts-ignore an undefined is ok in this case
+		const trueValue = OBJECT_TEST[key]
+		asserts.assertEquals(value.asString(), trueValue)
+
+		loopedKeys.push(key)
+	})
+
+	asserts.assertEquals(loopedKeys, trueKeys)
+})
+
+Deno.test('SafeUnknownObject.map maps', () => {
+	asserts.assertEquals(
+		Object.values(new SafeUnknownObject(OBJECT_TEST).map((value, key) => concatenate([key, value.asString()]))),
+		Object.entries(OBJECT_TEST).map(([key, value]) => concatenate([key, value])),
+	)
 })
