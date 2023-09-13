5193cb0f893e8985d8a8bc2938047769105275d6
Elijah Mooring <vehmloewff@gmail.com>
2023-09-13 12:42:05 -0700

    feat(cache): FsCache#clear and doc existing methods

--- cache.ts
+++ cache.ts
@@ -12,11 +12,13 @@ export class FsCache {
 		this.#dir = pathUtils.join(getCacheDirectory(), subDir)
 	}
 
+	/** Set a cache entry at `key`. Entry can be either a string or plain bytes */
 	async set(key: string, content: string | Uint8Array): Promise<void> {
 		const bytes = typeof content === 'string' ? this.#encoder.encode(content) : content
 		await writeBinary(pathUtils.join(this.#dir, key), bytes)
 	}
 
+	/** Get cache entry `key` as a string */
 	async getString(key: string): Promise<string | null> {
 		const bytes = await readBytes(key)
 		if (!bytes) return null
@@ -24,8 +26,14 @@ export class FsCache {
 		return this.#decoder.decode(bytes)
 	}
 
+	/** Get a cache entry `key` as bytes */
 	async getBytes(key: string): Promise<Uint8Array | null> {
 		return await readBytes(pathUtils.join(this.#dir, key))
+	}
+
+	/** Removes all entries under `scope` in cache dir */
+	async clear(): Promise<void> {
+		for await (const file of Deno.readDir(this.#dir)) await Deno.remove(pathUtils.join(this.#dir, file.name))
 	}
 }
 
@@ -37,5 +45,5 @@ function getCacheDirectory() {
 	const home = Deno.env.get('HOME')
 	if (!home) throw new Error('Could not detect users HOME directory')
 
-	return pathUtils.join(home, '.cache', 'stash_cache')
+	return pathUtils.join(home, '.cache', 'fs_cache')
 }
