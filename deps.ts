e2e13bad1cb174ba7bcf728d898a179b91f5f968
Elijah Mooring <vehmloewff@gmail.com>
2023-09-13 08:06:14 -0700

    ref(deps): remove the no longer needed `porter` dependency

--- deps.ts
+++ deps.ts
@@ -11,4 +11,3 @@ export * as cbor from 'https://deno.land/x/cbor@v1.5.4/index.js'
 
 // Development
 export * as asserts from 'https://deno.land/std@0.201.0/assert/mod.ts'
-export * as porter from 'https://deno.land/x/port@1.0.0/mod.ts'
