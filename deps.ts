0f5cbfcff6fd97b074cd5bfa4d3e8d0fa47430e8
Elijah Mooring <vehmloewff@gmail.com>
2023-10-25 22:53:06 -0700

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
 // Std
-export * as pathUtils from 'https://deno.land/std@0.201.0/path/mod.ts'
-export * as colors from 'https://deno.land/std@0.201.0/fmt/colors.ts'
-export * as base64 from 'https://deno.land/std@0.201.0/encoding/base64.ts'
-export * as streamUtils from 'https://deno.land/std@0.201.0/streams/mod.ts'
-export * as hexEncodingUtils from 'https://deno.land/std@0.201.0/encoding/hex.ts'
-export * as asyncUtils from 'https://deno.land/std@0.201.0/async/mod.ts'
+export * as pathUtils from 'https://deno.land/std@0.204.0/path/mod.ts'
+export * as colors from 'https://deno.land/std@0.204.0/fmt/colors.ts'
+export * as base64 from 'https://deno.land/std@0.204.0/encoding/base64.ts'
+export * as streamUtils from 'https://deno.land/std@0.204.0/streams/mod.ts'
+export * as hexEncodingUtils from 'https://deno.land/std@0.204.0/encoding/hex.ts'
+export * as asyncUtils from 'https://deno.land/std@0.204.0/async/mod.ts'
 
 // Third party
 export * as jwtCore from 'https://deno.land/x/djwt@v2.8/mod.ts'
 export * as cbor from 'https://deno.land/x/cbor@v1.5.4/index.js'
 
 // Development
-export * as asserts from 'https://deno.land/std@0.201.0/assert/mod.ts'
+export * as asserts from 'https://deno.land/std@0.204.0/assert/mod.ts'
 export * as porter from 'https://deno.land/x/port@1.0.0/mod.ts'
